5efb6ade5f17adeba5d37ecd2cb034f20ad64b0c
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2025-02-17 12:07:56 -0500

    do not use h5netcdf for xarray.tutorial fetching

--- tests/conftest.py
+++ tests/conftest.py
@@ -60,7 +60,7 @@ def ref_hist_sim_tuto():  # noqa: F841
     """
 
     def _ref_hist_sim_tuto(sim_offset=3, delta=0.1, smth_win=3, trend=True):
-        ds = xr.tutorial.open_dataset("air_temperature", engine="h5netcdf")
+        ds = xr.tutorial.open_dataset("air_temperature")
         ref = ds.air.resample(time="D").mean(keep_attrs=True)
         hist = ref.rolling(time=smth_win, min_periods=1).mean(keep_attrs=True) + delta
         hist.attrs["units"] = ref.attrs["units"]
